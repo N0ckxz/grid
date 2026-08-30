421f896391b8f5a425b7ea5796f6f2bfe96803a6
portatilNico <nicolasxls34@gmail.com>
2026-08-29 19:33:28 -0500

    Functioning grid (kind of, it aint visible but it is there)

--- main.cpp
+++ main.cpp
@@ -8,13 +8,34 @@ std::vector<unsigned char> canvasData;
 GLuint textureID;
 
 // initializing functions
-void handleCanvasResize(int width, int height);
+void handleCanvasResize(int width, int height); //since textureID is global now, we can just use width and height
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
 const unsigned int SCR_WIDTH = 300;
 const unsigned int SCR_HEIGHT = 300;
+
+//Shaders
+//I dont understand how this works, some day though
+const char *vertexShaderSource = "#version 460 core\n"
+    "layout (location = 0) in vec2 aPos;\n"
+    "layout (location = 1) in vec2 aTexCoord;\n"
+    "out vec2 TexCoord;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 0.0, 1.0);\n"
+    "   TexCoord = aTexCoord;\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 460 core\n"
+    "out vec4 FragColor;\n"
+    "in vec2 TexCoord;\n"
+    "uniform sampler2D ourTexture;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = texture(ourTexture, TexCoord);\n"
+    "}\n\0";
 
 int main()
 {
@@ -49,9 +70,59 @@ int main()
         return -1;
     }
 
-    textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
+    //Build and compile shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Clean up individual shader objects after linking
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //SETTING UP GEOMETRY (Quad, VBO, VAO)
+float quadVertices[] = {
+        // Positions   // TexCoords
+        -1.0f,  1.0f,  0.0f, 1.0f, // Top-Left
+        -1.0f, -1.0f,  0.0f, 0.0f, // Bottom-Left
+         1.0f, -1.0f,  1.0f, 0.0f, // Bottom-Right
+
+        -1.0f,  1.0f,  0.0f, 1.0f, // Top-Left
+         1.0f, -1.0f,  1.0f, 0.0f, // Bottom-Right
+         1.0f,  1.0f,  1.0f, 1.0f  // Top-Right
+    };
+
+//IMPORTANT: I dont understand any of this still, I should ask about it
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+
+    // Position Attribute (location = 0)
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // TexCoord Attribute (location = 1)
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+//Setting up canvas texture
+    
+    textureID; //Texture declaration
+    glGenTextures(1, &textureID); // ID and vector/pointer of textures
+    glBindTexture(GL_TEXTURE_2D, textureID); //We bind the texture to GPU VRAM
 
     // Set texture filtering (GL_NEAREST for crisp textures)
 
@@ -70,14 +141,24 @@ int main()
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // We append our little programs
+        glUseProgram(shaderProgram);
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -97,7 +178,10 @@ void handleCanvasResize(int width, int height)
 {
     // we resize the vector when needed
     canvasData.resize(width * height * 3);
-    
+
+    // Fill the vector with 255 (White) 
+    std::fill(canvasData.begin(), canvasData.end(), 255);
+
     // Bind and reallocate GPU texture
     // Upload the resized vector data to the GPU (vibe coded)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, canvasData.data());
